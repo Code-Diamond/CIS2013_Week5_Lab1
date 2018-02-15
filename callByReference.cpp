696a18068a3e100bd13bafe6a9dd398398895b0b
JaminROCK <KingJamin@hotmail.com>
2018-02-14 20:14:50 -0600

    Update callByReference.cpp
    whitespace edit

--- callByReference.cpp
+++ callByReference.cpp
@@ -6,14 +6,13 @@ int addNumbers(int&, int);
 
 int main()
 {
-
-		int firstNumber, secondNumber;
-		cout << "Enter first number: "; cin >> firstNumber;
-		cout << "Enter second number: "; cin >> secondNumber;
-			//demonstrates call by reference and pointers
-		cout << "The first result of calling addNumbers is: " << addNumbers(firstNumber, secondNumber) << endl;
-		cout << "The second result of calling addNumbers is: " << addNumbers(firstNumber, secondNumber) << endl;
-		cout << "The third result of calling addNumbers is: " << addNumbers(firstNumber, secondNumber) << endl;
+	int firstNumber, secondNumber;
+	cout << "Enter first number: "; cin >> firstNumber;
+	cout << "Enter second number: "; cin >> secondNumber;
+		//demonstrates call by reference and pointers
+	cout << "The first result of calling addNumbers is: " << addNumbers(firstNumber, secondNumber) << endl;
+	cout << "The second result of calling addNumbers is: " << addNumbers(firstNumber, secondNumber) << endl;
+	cout << "The third result of calling addNumbers is: " << addNumbers(firstNumber, secondNumber) << endl;
 	return 0;
 }
 
